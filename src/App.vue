14e0efc91fd6a865d6cd15fa326d173e27919b09
Huijiny <jiin20803@gmail.com>
2021-06-29 14:57:01 +0900

    Roll back to successed code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to heejin page" />
+    <HelloWorld msg="테스트 성공" />
   </div>
 </template>
 
